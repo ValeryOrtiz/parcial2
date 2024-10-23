78187e02fc271679a9276abd0ec831b3ec119f5b
Jhonatan David Vivas Arango <jhonatandavidvivasarango@gmail.com>
2024-10-22 21:48:32 -0500

    otro ejemplo que no entra

--- co.edu.uniquindio.parcial2 (2)/co.edu.uniquindio.parcial2/src/main/java/co/edu/uniquindio/parcial2/parcial2/abstractfactory/EmpleadoPermanenteFactory.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.parcial2.parcial2.abstractfactory;
-
-public class EmpleadoPermanenteFactory extends EmpleadoFactory {
-    @Override
-    public Empleado crearEmpleado(String nombre) {
-        return new EmpleadoPermanente(nombre);
-    }
-}
--- co.edu.uniquindio.parcial2 (2)/co.edu.uniquindio.parcial2/src/main/java/co/edu/uniquindio/parcial2/parcial2/abstractfactory/EmpleadoTemporalFactory.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.parcial2.parcial2.abstractfactory;
-
-public class EmpleadoTemporalFactory extends EmpleadoFactory {
-    @Override
-    public Empleado crearEmpleado(String nombre) {
-        return new EmpleadoTemporal(nombre);
-    }
-}
